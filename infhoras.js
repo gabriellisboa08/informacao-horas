11a8dd95309d24b21920ce77d5f895bfc0ca2f3e
Gabriel Lisboa da Silva <gabriellisboa08@gmail.com>
2021-04-15 07:00:43 -0300

    Aplicando condições

--- infhoras.js
+++ infhoras.js
@@ -1,11 +1,25 @@
 function carregar () {
-  let divmsg = document.getElementById('msg')
+let divmsg = document.getElementById('msg')
 let divimg = document.getElementById('img')
 let data = new Date() //variável resgatando informações do sistema (hora,data,etc...)
 let hora = data.getHours() //resgatando hora  
- 
+let  min = data.getMinutes()
+
+if (hora >= 0 && hora < 12 ){
+  divmsg.innerText = `Bom dia! Agora são exatamente ${hora}:${min} horas `
+  divimg.src = 'manhã.jpg'
+  
+} else if(hora >= 12 && hora < 18 ) {
+  divmsg.innerText = `Boa tarde! Agora são exatamente ${hora}:${min} horas`
+  divimg.src = 'tarde.jpg'
+
+}else if (hora >= 18 && hora <= 23) {
+  divmsg.innerText = `Boa noite! Agora são exatamente ${hora}:${min} horas`
+
+}else {
+  divmsg.innerText = '[ERRO] Hora invalida!'
 }
 
-
+}
 
 
