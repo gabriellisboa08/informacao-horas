8794f27618aeab8d295bc2b8dbcd93fa71fdaf58
Gabriel Lisboa da Silva <gabriellisboa08@gmail.com>
2021-04-14 21:17:03 -0300

    update
    foi adicionado as fotos que irão compor o programa e feito alguns ajustes em JS e HTML

---
+++ infhoras.js
@@ -0,0 +1,11 @@
+function carregar () {
+  let divmsg = document.getElementById('msg')
+let divimg = document.getElementById('img')
+let data = new Date() //variável resgatando informações do sistema (hora,data,etc...)
+let hora = data.getHours() //resgatando hora  
+ 
+}
+
+
+
+
