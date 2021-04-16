532a7bdad7386211323bc8df2eae3d7199cddb48
Gabriel Lisboa da Silva <gabriellisboa08@gmail.com>
2021-04-15 21:10:32 -0300

    fim do mini-projeto
    foi melhorado as imagens e feito alguns ajustes em CSS

--- infhoras.js
+++ infhoras.js
@@ -2,22 +2,19 @@ function carregar () {
 let divmsg = document.getElementById('msg')
 let foto = document.getElementById('foto')
 let data = new Date() //variável resgatando informações do sistema (hora,data,etc...)
-//let hora = data.getHours() //resgatando hora  
+let hora = data.getHours() //resgatan
 let  min = data.getMinutes()
-hora = 16
 if (hora >= 0 && hora < 12 ){
-  divmsg.innerText = `Bom dia! Agora são exatamente ${hora}:${min} horas `
-  foto.src = 'manha.jpg' 
-  document.body.style.background = '#e2cd9f'
+  divmsg.innerText = `Bom dia! Agora são exatamente ${hora}:${min}:${sec} horas `
+  foto.src = 'manha.png' //carregando a imagem dentro da div
+  document.body.style.background = '#e2cd9f' //aolicando CSS via JS
 } else if(hora >= 12 && hora < 18 ) {
   divmsg.innerText = `Boa tarde! Agora são exatamente ${hora}:${min} horas`
-  foto.src = 'tarde.jpg'
+  foto.src = 'tarde.png'
   document.body.style.background = '#b9846f'
-  
-
 }else if (hora >= 18 && hora <= 23) {
   divmsg.innerText = `Boa noite! Agora são exatamente ${hora}:${min} horas`
-  foto.src = 'noite.jpg'
+  foto.src = 'noite.png'
   document.body.style.background = '#515154'
 }else {
   divmsg.innerText = '[ERRO] Hora invalida!'
