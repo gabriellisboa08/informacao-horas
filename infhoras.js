18b2968869c82046b503e4d837643a4cf8b2360e
Gabriel Lisboa da Silva <gabriellisboa08@gmail.com>
2021-10-20 11:55:54 -0300

    updateinfhora.js
    Atualizando o código com funções para deixá-lo mais limpo.

--- infhoras.js
+++ infhoras.js
@@ -1,25 +1,51 @@
 function carregar () {
-let divmsg = document.getElementById('msg')
-let foto = document.getElementById('foto')
-let data = new Date() //variável resgatando informações do sistema (hora,data,etc...)
-let hora = data.getHours() //resgatan
-let  min = data.getMinutes()
-if (hora >= 0 && hora < 12 ){
-  divmsg.innerText = `Bom dia! Agora são exatamente ${hora}:${min}:${sec} horas `
-  foto.src = 'manha.png' //carregando a imagem dentro da div
-  document.body.style.background = '#e2cd9f' //aolicando CSS via JS
-} else if(hora >= 12 && hora < 18 ) {
-  divmsg.innerText = `Boa tarde! Agora são exatamente ${hora}:${min} horas`
-  foto.src = 'tarde.png'
-  document.body.style.background = '#b9846f'
-}else if (hora >= 18 && hora <= 23) {
-  divmsg.innerText = `Boa noite! Agora são exatamente ${hora}:${min} horas`
-  foto.src = 'noite.png'
-  document.body.style.background = '#515154'
-}else {
-  divmsg.innerText = '[ERRO] Hora invalida!'
-}
+var divmsg = document.getElementById('msg')
+var foto = document.getElementById('foto')
 
 }
+VerificarHora()
+function resgateHora() {
+  let data = new Date()
+  return data
+}
+function formatarHora () {
+  let data = resgateHora()
+  let hora = data.getHours() //resgatando
+  //let horaFormatada = (hora.length == 1)? "0" + hora : hora
+  return hora
+}
+function formatarMin() {
+  let data = resgateHora()
+  let  min = data.getMinutes()
+  //let minFormatado = (min.length == 1 ) ? "0" + min : min
+  return min
+}
+function formatarSec() {
+  let data = resgateHora()
+  let sec = data.getSeconds()
+  return sec
+}
+
+function VerificarHora() {
+  if (formatarHora() >= 0 && formatarHora() < 12 ){
+    var divmsg = document.getElementById('msg')
+var foto = document.getElementById('foto')
+    divmsg.innerText = `Bom dia! Agora são exatamente ${formatarHora()}:${formatarMin()}:${formatarSec()} horas `
+    foto.src = 'manha.png' //carregando a imagem dentro da div
+    document.body.style.background = '#e2cd9f' //aolicando CSS via JS
+  } else if(formatarHora() >= 12 && formatarHora() < 18 ) {
+    divmsg.innerText = `Boa tarde! Agora são exatamente ${formatarHora()}:${formatarMin()}:${formatarSec()} horas`
+    foto.src = 'tarde.png'
+    document.body.style.background = '#b9846f'
+  }else if (formatarHora() >= 18 && formatarHora() <= 23) {
+    divmsg.innerText = `Boa noite! Agora são exatamente ${formatarHora()}:${formatarMin()}:${formatarSec()} horas`
+    foto.src = 'noite.png'
+    document.body.style.background = '#515154'
+  }
+
+}
+
+
+
 
 
